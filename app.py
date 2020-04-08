5b70ecc0d73521f0d7af292728b1e615c8ef916f
Rucha Deshpande <rucha.d1690@gmail.com>
2020-04-08 11:54:25 +0530

    login and registration page

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template,session
+from flask import Flask, render_template,session, flash, redirect, request, url_for
 from forms import RegistrationForm, LoginForm
-from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
-import redis
 import os
 
 app = Flask(__name__)
@@ -15,11 +13,20 @@ def index():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm('/signup')
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('category'))
     return render_template('signup.html',title='Register', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm('/login')
+    if form.validate_on_submit():
+        if form.email.data == 'email' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
