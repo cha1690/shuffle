4b9868151043b2b206978d5fe12e62356fe1c43b
Rucha Deshpande <rucha.d1690@gmail.com>
2020-04-09 15:57:07 +0530

    connected form with the database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template,session, flash, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
@@ -9,6 +10,21 @@ csrf = CSRFProtect(app)
 
 app.config.update(SECRET_KEY=os.urandom(24))
 app.config['CSRF_ENABLED'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    category = db.Column(db.String(60), nullable=False)
+#     posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 @app.route('/')
 def index():
@@ -27,7 +43,7 @@ def signup():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('category'))
+        return redirect(url_for('home'))
     return render_template('signup.html',title='Register', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
--- forms.py
+++ forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -11,8 +14,12 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
+    categories = [('1', 'Cooking & Health'), ('2', 'Beauty & Fashion'), ('3', 'Music'), ('4', 'Pets & Animals'),
+               ('5', 'Sports'), ('6', 'Short Movies'), ('7', 'Comedy'), ('8', 'Entertainment'),
+               ('9', 'News & Politics'), ('10', 'Education'), ('11', 'Tech'),
+               ('12', 'Travel & Events'), ('13', 'Videoblogging')]
+    category = MultiCheckboxField('Category', choices=categories)
     submit = SubmitField('Sign Up')
-
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
