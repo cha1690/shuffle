7be5edaa65a6b494dc300b3b3bc3bf84da151cc6
Rucha Deshpande <rucha.d1690@gmail.com>
2020-05-18 18:15:26 +0530

    added csrf time limit

--- shuffle/__init__.py
+++ shuffle/__init__.py
@@ -16,6 +16,7 @@ heroku = Heroku(app)
 csrf.init_app(app)
 app.config.update(SECRET_KEY=os.urandom(24))
 app.config['CSRF_ENABLED'] = True
+app.config['WTF_CSRF_TIME_LIMIT'] = None
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kuyshutiszlleb:e070539f2ea1dc2c6a6424025661ca7e6a76c8a3d465834fe196ab0da0409b76@ec2-184-72-235-80.compute-1.amazonaws.com:5432/da40pli2qnlfj2'
@@ -26,6 +27,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 login_manager.session_protection = 'basic'
+
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
