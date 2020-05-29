eb5b8d2bb5345104b16855ef636f1b023e139bce
Rucha Deshpande <rucha.d1690@gmail.com>
2020-05-29 17:04:19 +0530

    Update __init__.py

--- shuffle/__init__.py
+++ shuffle/__init__.py
@@ -19,7 +19,7 @@ app.config['CSRF_ENABLED'] = True
 app.config['WTF_CSRF_TIME_LIMIT'] = None
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kuyshutiszlleb:e070539f2ea1dc2c6a6424025661ca7e6a76c8a3d465834fe196ab0da0409b76@ec2-184-72-235-80.compute-1.amazonaws.com:5432/da40pli2qnlfj2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'URI'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -31,10 +31,10 @@ login_manager.session_protection = 'basic'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'shuffleapp8@gmail.com'
-app.config['MAIL_PASSWORD'] = '1DpVX5jnQfea'
+app.config['MAIL_USERNAME'] = 'email@gmail.com'
+app.config['MAIL_PASSWORD'] = 'password'
 mail = Mail(app)
-DEVELOPER_KEY = "AIzaSyDEgXt5MtD9-J3B1B5x9KlxOnP1etevG6w"
+DEVELOPER_KEY = "developer key"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 youtube_object = build(YOUTUBE_API_SERVICE_NAME,
